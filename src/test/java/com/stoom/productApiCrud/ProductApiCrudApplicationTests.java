52e6922957a2965eaaa032540d91dc6259264e63
devCaiqueDePaula <dev.caiquedepaula@gmail.com>
2024-08-05 16:33:41 -0300

    Initial

---
+++ src/test/java/com/stoom/productApiCrud/ProductApiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stoom.productApiCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
